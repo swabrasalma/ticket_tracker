50eaaddaf59a437b6b8d3373546e5c85b92f09c8
swabrasalma <swabrakaterega@gmail.com>
2019-07-12 17:33:23 +0300

    initial works

--- api/models.py
+++ api/models.py
@@ -15,16 +15,13 @@ class User(AbstractUser):
 
 class Request(models.Model):
     id = models.BigAutoField(primary_key=True)
-    requestor_name = models.CharField(max_length = 200)
-    phone_number = models.CharField(max_length = 200)
-    submission_comments = models.TextField()
-    attachments = models.FileField(upload_to='requests')
-    status = models.CharField(max_length=20, default='New')
-    amount_paid_by_customer = models.CharField(max_length=20)
-    payment_details = models.TextField()
-    qc_admin_comments = models.TextField()    
+    company_to_train = models.CharField(max_length = 200)
+    product_to_be_trained = models.CharField(max_length = 200)
+    training_date_and_time = models.DateTimeField()
+    number_of_people_to_train = models.IntegerField(default=0)
+    status = models.CharField(max_length=20, default='Pending')
     added_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     class Meta:
         db_table = 'requests'
 
@@ -60,5 +57,6 @@ class EditLog(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     class Meta:
         db_table = 'edit_logs'
+
 
 
